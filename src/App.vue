49d6ae1c48a904301e6d02b0d9f243b757df7258
nataliia-v <nataly.verbenskaya@gmail.com>
2025-05-15 14:16:24 +0300

    code refactoring, added todo item component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import StatusFilter from './components/StatusFilter.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   components: {
     Filter: StatusFilter,
+    TodoItem,
   },
   data() {
     let todos = []
@@ -67,42 +69,13 @@ export default {
       </header>
 
       <section class="todoapp__main">
-        <div
-          class="todo"
-          :class="{ completed: todo.completed }"
+        <TodoItem
           v-for="(todo, index) of todos"
           :key="todo.id"
-        >
-          <label class="todo__status-label">
-            <!-- <input
-              type="checkbox"
-              class="todo__status"
-              :checked="todo.completed"
-              @change="todo.completed = !todo.completed"
-            /> -->
-            <input type="checkbox" class="todo__status" v-model="todo.completed" />
-          </label>
-
-          <form v-if="false">
-            <input
-              type="text"
-              class="todo__title-field"
-              placeholder="Empty todo will be deleted"
-              value="Todo is being edited now"
-            />
-          </form>
-
-          <template v-else>
-            <span class="todo__title">{{ todo.title }}</span>
-
-            <button class="todo__remove" @click="todos.splice(index, 1)">x</button>
-          </template>
-
-          <div class="modal overlay" :class="{ 'is-active': false }">
-            <div class="modal-background has-background-white-ter"></div>
-            <div class="loader"></div>
-          </div>
-        </div>
+          :todo="todo"
+          @update="todos[index] = $event"
+          @delete="todos.splice(index, 1)"
+        />
       </section>
 
       <footer class="todoapp__footer">
